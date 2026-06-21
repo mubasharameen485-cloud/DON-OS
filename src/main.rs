ee454a0647c0dc906eaf16b1d9f4e0f90773b2f7
mubasharameen485-cloud <mubasharameen485@gmail.com>
2026-06-21 20:37:32 +0500

    feat: setup enterprise microkernel services architecture

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-// src/main.rs
 
 #![no_std]
 #![no_main]
 
-// Hum apni library (lib.rs) se components import kar rahe hain
+
 use my_rust_os::services;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // ---------------------------------------------------------
-    // Yahan DON-OS ka boot sequence start hoga.
-    // Future mein hum yahan likhenge:
-    // services::io_service::init();
-    // services::memory_service::init();
-    // ---------------------------------------------------------
     
-    // OS ko zinda rakhne ke liye infinite loop
     loop {}
 }
